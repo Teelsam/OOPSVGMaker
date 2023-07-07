49fa675d6ed4d793cc8c2dfdf1d8b29a6610f05e
Sam Teel <sjkteel@hotmail.com>
2023-07-07 18:41:04 -0400

    added connection through shapes to index

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function init() {//makes sure the entire code is initiallized.
     //loads the needed content to run this code
     const inquirer = require('inquirer');
     // const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
-    const jest = require('jest');
     const fs = require('fs');
-    const genSVG = require('./lib/shapes');
+    const {Triangle,Circle,Square} = require('./lib/shapes');
+    
 
     inquirer
         .prompt([{ //asks the user quesitons in the terminal
@@ -37,24 +37,28 @@ function init() {//makes sure the entire code is initiallized.
         })
 
     function svgInfo(data) { // this file sets up the content for the next function to create SVG, by checking for shapeinfo
+        let shape;
         if (data.shape === 'circle') {
+            shape = new Circle(data.shapeColor);
             return `
     <svg version="1.1" width="500" height="500" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="200" cy="100" r="100" fill ="${data.shapeColor}"/>
+    ${shape.render()}
     <text text-anchor="middle" x="200" y="100" font-size="60" fill="${data.textColor}">${data.Characters}</text>
     </svg> `
         }
         else if (data.shape === 'triangle') {
+            shape = new Triangle(data.shapeColor);
             return `
         <svg version="1.1" width="500" height="500" xmlns="http://www.w3.org/2000/svg">
-        <polygon points="0,300 300,300 150,0" fill="${data.shapeColor}"/>
+        ${shape.render()}
         <text text-anchor="middle"  x="150" y="200" font-size="60" fill="${data.textColor}">${data.Characters}</text>
         </svg> `
         }
         else if (data.shape === 'square') {
+            shape = new Square(data.shapeColor);
             return `
         <svg version="1.1" width="500" height="500" xmlns="http://www.w3.org/2000/svg">
-        <rect x="90" y="25" width="300" height="300" fill="${data.shapeColor}"/>
+        ${shape.render()}
         <text text-anchor="middle" x="225" y="150" font-size="60" fill="${data.textColor}">${data.Characters}</text>
         </svg> `
         }
