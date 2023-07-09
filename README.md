4869019eec61b387b0ed6a28c455a70faa0795b3
Sam Teel <sjkteel@hotmail.com>
2023-07-09 18:33:15 -0400

    added comments and wrote readme

--- README.md
+++ README.md
@@ -1,12 +1,30 @@
 # OOPSVGMaker
 
+## Table of Contents
+-[Description](#description)
+-[Installation](#installation)
+-[Usage](#usage)
+-[Questions](#questions)
+-[References](#references)
+
+## Description 
+
+Here we have some code for creating SVG graphics through inquirer prompted data. Which uses Object Oriented Programming fundamentals and Jest Product testing to make happen. 
+
+## Installation 
+Inorder for this application to work you have to have the npm package inquirer downloaded.This is the package that allows for neat and tidy questions to be prompted in the terminal. Its installation and information can be found [here](https://www.npmjs.com/package/inquirer/v/8.2.4). As well as you need  to know Nodejs which can be found [here2](https://nodejs.org/en). 
+
+
+## Usage
+
+Opening the code in your IDE, and running `npm install` in the terminal, you will get the packages you need and should be able to run `node index.js`. Answering the terminals questions a new file will be created which you can open in a browser to see created SVG logo created. 
+
+
+## Questions 
+[github  link](https://github.com/teelsam)
+
+
+[My Email]( sjkteel@gmail.com)
 
 
 
-
-
-
-
-## Resources 
-
-[Inquirer maxlength input prompt] (https://www.npmjs.com/package/inquirer-maxlength-input-prompt)
--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function init() {//makes sure the entire code is initiallized.
     //loads the needed content to run this code
     const inquirer = require('inquirer');
     // const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
-    const fs = require('fs');
-    const {Triangle,Circle,Square} = require('./lib/shapes');
-    
+    const fs = require('fs');// require the package to handle file creation
+    const { Triangle, Circle, Square } = require('./lib/shapes');// import the class objs from shapes
+
 
     inquirer
         .prompt([{ //asks the user quesitons in the terminal
@@ -32,42 +32,42 @@ function init() {//makes sure the entire code is initiallized.
             name: 'shapeColor',
         }])
         .then(data => {//sends the data to the function writeFile
-            console.log('data is: ', data);
-            writeToFile(data);
+            console.log('data is: ', data); // prints what the data is saved as.
+            writeToFile(data);//gives writeToFile the users input data
         })
 
     function svgInfo(data) { // this file sets up the content for the next function to create SVG, by checking for shapeinfo
         let shape;
-        if (data.shape === 'circle') {
+        if (data.shape === 'circle') { //checks if data.shape answer is circle
             shape = new Circle(data.shapeColor);
             return `
     <svg version="1.1" width="500" height="500" xmlns="http://www.w3.org/2000/svg">
     ${shape.render()}
     <text text-anchor="middle" x="200" y="100" font-size="60" fill="${data.textColor}">${data.Characters}</text>
-    </svg> `
+    </svg> ` // creates the info for svg shape and uses the imported render function to give shape specifics
         }
-        else if (data.shape === 'triangle') {
+        else if (data.shape === 'triangle') {//checks if data.shape answer is triangle
             shape = new Triangle(data.shapeColor);
             return `
         <svg version="1.1" width="500" height="500" xmlns="http://www.w3.org/2000/svg">
         ${shape.render()}
         <text text-anchor="middle"  x="150" y="200" font-size="60" fill="${data.textColor}">${data.Characters}</text>
-        </svg> `
+        </svg> `// creates the info for svg shape and uses the imported render function to give shape specifics
         }
-        else if (data.shape === 'square') {
+        else if (data.shape === 'square') {//checks if data.shape answer is square.
             shape = new Square(data.shapeColor);
             return `
         <svg version="1.1" width="500" height="500" xmlns="http://www.w3.org/2000/svg">
         ${shape.render()}
         <text text-anchor="middle" x="225" y="150" font-size="60" fill="${data.textColor}">${data.Characters}</text>
-        </svg> `
+        </svg> `// creates the info for svg shape and uses the imported render function to give shape specifics
         }
     }
     function writeToFile(data) { //after SVG info picks which shape to build, this function writes that new files content
         const SVG = svgInfo(data);
         fs.writeFile(
             'logo.svg', SVG, err => err ? console.log(err) : console.log('file created successfully')
-        );
+        );// writes a file called logo.svg and prints to console if correcct "file created".
     }
 };
 init();
